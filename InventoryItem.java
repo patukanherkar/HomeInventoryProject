8f24225fffb7f2bf24238b665d891eb90536845d
patukanherkar <90126726+patukanherkar@users.noreply.github.com>
2021-10-05 23:21:05 +0530

    Add files via upload

---
+++ InventoryItem.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package HomeInventory;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class InventoryItem {
+     public String description;
+     public String location;
+     public boolean marked;
+     public String serialNumber;
+     public String purchasePrice;
+     public String purchaseDate;
+     public String purchaseLocation;
+     public String note;
+     public String photoFile;
+}
+
